b4ae2476b56db309c4c47eaedcbe4ee295052f8c
Bhuvan S A <bhuvansaraj2003@gmail.com>
2023-09-09 12:35:40 +0530

    we

--- components/Hero.js
+++ components/Hero.js
@@ -29,44 +29,42 @@ const Hero = ({
 
   return (
     <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="about">
-      <ScrollAnimationWrapper>
-        <motion.div
-          className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
-          variants={scrollAnimation}
-        >
-          <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-              Unlocking Innovation:{" "}
-              <strong className=" text-orange-500">
-                Join the Ideathon Revolution
-                {/* <TypeAnimation
+      <motion.div
+        className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
+        variants={scrollAnimation}
+      >
+        <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
+          <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+            Unlocking Innovation:{" "}
+            <strong className=" text-orange-500">
+              Join the Ideathon Revolution
+              {/* <TypeAnimation
                   className="box-content h-10"
                   sequence={["Join the Ideathon Revolution", () => {}]}
                   wrapper="span"
                   cursor={false}
                 /> */}
-              </strong>
-            </h1>
-            <p className="text-black-500 mt-4 mb-6">
-              Fostering Innovation through Collaboration, Technology, and
-              Creative Thinking
-            </p>
-            <ButtonPrimary>Register Now </ButtonPrimary>
-          </div>
-          <div className="flex w-full">
-            <motion.div className="h-full w-full" variants={scrollAnimation}>
-              <Image
-                src="/assets/Illustration3.png"
-                alt="VPN Illustrasi"
-                quality={100}
-                width={612}
-                height={383}
-                layout="responsive"
-              />
-            </motion.div>
-          </div>
-        </motion.div>
-      </ScrollAnimationWrapper>
+            </strong>
+          </h1>
+          <p className="text-black-500 mt-4 mb-6">
+            Fostering Innovation through Collaboration, Technology, and Creative
+            Thinking
+          </p>
+          <ButtonPrimary>Register Now </ButtonPrimary>
+        </div>
+        <div className="flex w-full">
+          <motion.div className="h-full w-full" variants={scrollAnimation}>
+            <Image
+              src="/assets/Illustration3.png"
+              alt="VPN Illustrasi"
+              quality={100}
+              width={612}
+              height={383}
+              layout="responsive"
+            />
+          </motion.div>
+        </div>
+      </motion.div>
       <div className="relative w-full flex">
         <ScrollAnimationWrapper className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
           {listUser.map((listUsers, index) => (
