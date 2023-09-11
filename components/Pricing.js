64cb1528f9748f0bb3aaae62a47dec033600fd1f
Prajwal2304 <120559065+Prajwal2304@users.noreply.github.com>
2023-09-11 23:57:24 +0530

    Update Pricing.js

--- components/Pricing.js
+++ components/Pricing.js
@@ -277,7 +277,7 @@ const Pricing = () => {
                   Register Now!
                 </h5>
               </div>
-              <ButtonPrimary>Register</ButtonPrimary>
+              <ButtonPrimary><button onclick="https://forms.gle/smYEHjf92AcSkyZQ8">Register</button>Register</ButtonPrimary>
             </div>
             <div
               className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
