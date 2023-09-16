22f377a7c4bf4ba0b4a28103455135e9cce0a84e
Bhuvan S A <bhuvansaraj2003@gmail.com>
2023-09-17 02:06:44 +0530

    Added theme cards v2

--- components/Pricing.js
+++ components/Pricing.js
@@ -274,34 +274,36 @@ const Pricing = () => {
         <div className="box-content h-24"></div>
         <div className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
           <div className="flex flex-col w-full"></div>
-          <ScrollAnimationWrapper className="relative w-full my-16">
-            <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
-              <div className="mb-6">
-                <h3 className="text-xl font-semibold mb-2 text-orange-500">
-                  Themes
-                </h3>
-                <div className="grid grid-rows-2 grid-flow-col gap-10">
-                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="mb-6">
+            <h2 className="text-3xl font-semibold text-center mb-8 text-orange-500">
+              Themes
+            </h2>
+            <div className="grid sm:grid-rows-2 grid-rows-4 grid-flow-col sm:gap-0 justify-evenly">
+              <ScrollAnimationWrapper className="relative w-full my-16">
+                <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
+                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                      <img
-                        class="rounded-t-lg"
-                        src="/docs/images/blog/image-1.jpg"
-                        alt=""
+                      <Image
+                        className="rounded-t-lg"
+                        src="/assets/SmartCity.webp"
+                        alt="smart-city image"
+                        height={328}
+                        width={500}
                       />
                     </a>
-                    <div class="p-5">
+                    <div className="p-5">
                       <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                           AI for sustainable cities and communities
                         </h5>
                       </a>
                       <a
                         href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       >
                         Read more
                         <svg
-                          class="w-3.5 h-3.5 ml-2"
+                          className="w-3.5 h-3.5 ml-2"
                           aria-hidden="true"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -309,36 +311,42 @@ const Pricing = () => {
                         >
                           <path
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M1 5h12m0 0L9 1m4 4L9 9"
                           />
                         </svg>
                       </a>
                     </div>
                   </div>
-                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                </motion.div>
+              </ScrollAnimationWrapper>
+              <ScrollAnimationWrapper className="relative w-full my-16">
+                <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
+                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                      <img
-                        class="rounded-t-lg"
-                        src="/docs/images/blog/image-1.jpg"
-                        alt=""
+                      <Image
+                        className="rounded-t-lg"
+                        src="/assets/SmartEducation.webp"
+                        alt="smart-education image"
+                        height={328}
+                        width={500}
                       />
                     </a>
-                    <div class="p-5">
+                    <div className="p-5">
                       <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                           AI for quality education for sustainable development
                         </h5>
                       </a>
                       <a
                         href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       >
                         Read more
                         <svg
-                          class="w-3.5 h-3.5 ml-2"
+                          className="w-3.5 h-3.5 ml-2"
                           aria-hidden="true"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -346,37 +354,42 @@ const Pricing = () => {
                         >
                           <path
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M1 5h12m0 0L9 1m4 4L9 9"
                           />
                         </svg>
                       </a>
                     </div>
                   </div>
-
-                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                </motion.div>
+              </ScrollAnimationWrapper>
+              <ScrollAnimationWrapper className="relative w-full my-16">
+                <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
+                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                      <img
-                        class="rounded-t-lg"
-                        src="/docs/images/blog/image-1.jpg"
-                        alt=""
+                      <Image
+                        className="rounded-t-lg"
+                        src="/assets/SmartHealthcare.webp"
+                        alt="smart-healthcare image"
+                        height={328}
+                        width={500}
                       />
                     </a>
-                    <div class="p-5">
+                    <div className="p-5">
                       <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                           AI for good health and wellbeing
                         </h5>
                       </a>
                       <a
                         href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       >
                         Read more
                         <svg
-                          class="w-3.5 h-3.5 ml-2"
+                          className="w-3.5 h-3.5 ml-2"
                           aria-hidden="true"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -384,37 +397,43 @@ const Pricing = () => {
                         >
                           <path
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M1 5h12m0 0L9 1m4 4L9 9"
                           />
                         </svg>
                       </a>
                     </div>
                   </div>
-                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                </motion.div>
+              </ScrollAnimationWrapper>
+              <ScrollAnimationWrapper className="relative w-full my-16">
+                <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
+                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                      <img
-                        class="rounded-t-lg"
-                        src="/docs/images/blog/image-1.jpg"
-                        alt=""
+                      <Image
+                        className="rounded-t-lg"
+                        src="/assets/Agriculture.webp"
+                        alt="smart agriculture image"
+                        height={328}
+                        width={500}
                       />
                     </a>
-                    <div class="p-5">
+                    <div className="p-5">
                       <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                           AI in agriculture for sustainable consumption and
                           production
                         </h5>
                       </a>
                       <a
                         href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       >
                         Read more
                         <svg
-                          class="w-3.5 h-3.5 ml-2"
+                          className="w-3.5 h-3.5 ml-2"
                           aria-hidden="true"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -422,25 +441,26 @@ const Pricing = () => {
                         >
                           <path
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M1 5h12m0 0L9 1m4 4L9 9"
                           />
                         </svg>
                       </a>
                     </div>
                   </div>
-                </div>
-              </div>
-            </motion.div>
-          </ScrollAnimationWrapper>
+                </motion.div>
+              </ScrollAnimationWrapper>
+            </div>
+          </div>
         </div>
       </div>
       <div
         className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full justify-center"
         id="testimoni"
       >
+        <h2 className="text-xl font-semibold mb-2 text-orange-500">Details</h2>
         <div className="mb-6">
           <h3 className="text-xl font-semibold mb-2 text-orange-500">
             Steps to participate in AIdeathon
---
+++ pages/register.js
@@ -0,0 +1,19 @@
+// pages/register.js
+
+import React from "react";
+
+const Register = () => {
+  // Your registration page content and logic here
+
+  return (
+    <div>
+      <h1>
+        Please download the formats listed below, fill and upload the same in
+        form
+      </h1>
+      {/* Add your registration form or content here */}
+    </div>
+  );
+};
+
+export default Register;
