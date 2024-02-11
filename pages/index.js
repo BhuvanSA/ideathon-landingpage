24fc5276d8755af52215dde7319e3429354c2b81
Bhuvan S A <bhuvansaraj2003@gmail.com>
2024-02-11 22:55:46 +0530

    Add analytics

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Pricing from "../components/Pricing";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
 import SeoHead from "../components/SeoHead";
+import { Analytics } from "@vercel/analytics";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Feature />
         <Pricing />
       </Layout>
+      <Analytics />
     </>
   );
 }
